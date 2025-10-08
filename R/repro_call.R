c30848e5dc7173796e68e41ca2c444c54dda1dd6
Ashley Baldry <ashley.baldry@ascent.io>
2025-10-08 14:57:17 +0100

    Enable multiple if/else statements

--- R/repro_call.R
+++ R/repro_call.R
@@ -9,8 +9,11 @@ S7::method(repro_chunk, S7::class_call) <- function(x, ..., repro_code = Repro()
   } else if (rlang::is_call(x, "if")) {
     if_args <- rlang::call_args(x)
     check <- eval(if_args[[1]], envir = env)
+    # Adapts for if ... else if ... else
+    if (!check && rlang::is_call(if_args[[3]], "if")) {
+      return(repro_chunk(if_args[[3]], env = env))
+    }
     check_calls <- purrr::map(as.list(if_args[[3 - check]])[-1], repro_chunk, env = env)
-
     repro_code@packages <- purrr::map(check_calls, "packages") |> unlist()
     repro_code@prerequisites <- purrr::map(check_calls, "prerequisites") |>
       purrr::discard(identical, list()) |>
